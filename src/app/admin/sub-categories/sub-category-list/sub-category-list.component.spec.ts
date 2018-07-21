8c6d1f49dcce8e6fa1497795379a6a1bf9afd053
himabindu <himabindu@binduwork-pc.com>
2018-07-21 18:28:16 +0530

    commited

--- src/app/admin/sub-categories/sub-category-list/sub-category-list.component.spec.ts
+++ src/app/admin/sub-categories/sub-category-list/sub-category-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SubSectorListComponent } from './sub-sector-list.component';
+import { SubSectorListComponent } from './sub-category-list.component';
 
 describe('SubSectorListComponent', () => {
   let component: SubSectorListComponent;
